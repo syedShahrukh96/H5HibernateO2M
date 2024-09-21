f2caa6541c82b1dd7380d422ef0e9377d5da73ce
syedShahrukh96 <syed.shahrukh33@gmail.com>
2024-09-21 12:26:00 -0500

    added onetoMany(List<Address> address) and manytoOne(employee)

---
+++ H5HibernateO2M/src/main/java/Com/shahrukh/hibernate/FirstProject/Address.java
@@ -0,0 +1,258 @@
+package Com.shahrukh.hibernate.FirstProject;
+
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "h3_address")
+public class Address {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int addressId;
+	
+	private String city;
+	private String state;
+	private int pincode;
+	
+	//here with this only address will be delected not the employee object
+	@ManyToOne(cascade = {
+			CascadeType.DETACH, 
+			CascadeType.MERGE, 
+			CascadeType.PERSIST, 
+			CascadeType.REFRESH})
+	@JoinColumn(name="EMP-ID")
+	private Employee employee;
+	
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	
+	
+	
+
+
+
+
+	public Address(int addressId, String city, String state, int pincode, Employee employee) {
+		super();
+		this.addressId = addressId;
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+		this.employee = employee;
+	}
+	
+	
+	//without address id, when we are inserting we dont have ID
+	public Address(String city, String state, int pincode, Employee employee) {
+		super();
+		
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+		this.employee = employee;
+	}
+
+	//when we need employee object 
+	public Address(String city, String state, int pincode) {
+		super();
+		
+		this.city = city;
+		this.state = state;
+		this.pincode = pincode;
+	
+	}
+
+	
+	
+	
+
+
+
+
+
+
+
+
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+
+
+
+
+
+
+
+
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+
+
+
+
+
+
+
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+
+
+
+
+
+
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+
+
+
+
+
+
+
+
+	public String getState() {
+		return state;
+	}
+
+
+
+
+
+
+
+
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+
+
+
+
+
+
+
+
+	public int getPincode() {
+		return pincode;
+	}
+
+
+
+
+
+
+
+
+
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+
+
+
+
+
+
+
+
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+
+
+
+
+
+
+
+
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+
+
+
+
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Address [addressId=" + addressId + ", city=" + city + ", state=" + state + ", pincode=" + pincode
+				+ "]";
+	}
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+
+}
---
+++ H5HibernateO2M/src/main/java/Com/shahrukh/hibernate/FirstProject/App.java
@@ -0,0 +1,117 @@
+package Com.shahrukh.hibernate.FirstProject;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	
+    	
+		/*
+		 * This line creates a Configuration object and configures it using the
+		 * hibernate.cfg.xml file (or other configuration files specified). This
+		 * configuration file contains all the database connection settings, mappings,
+		 * and other Hibernate-related configurations.
+		 */
+    	Configuration cfg = new Configuration().configure();
+    	
+		/*
+		 * The SessionFactory is a factory for Session objects. It is a heavyweight
+		 * object that is created once during the application startup and used to create
+		 * Session objects throughout the application. It is built using the
+		 * configuration from the Configuration object.
+		 */
+    	SessionFactory factory =cfg.buildSessionFactory();
+    	
+		/*
+		 * This line opens a new Session from the SessionFactory. A Session is the
+		 * primary nterface for performing CRUD (Create, Read, Update, Delete)
+		 * operations on the database using Hibernate. It is a lightweight object that
+		 * should be created and closed with each database transaction.
+		 */
+    	Session session = factory.openSession();
+    	
+		/*
+		 * This line begins a new transaction. In Hibernate, you need to explicitly
+		 * start and commit transactions to ensure that your operations (like saving an
+		 * entity) are executed atomically and consistently.
+		 */
+    	Transaction tx = session.beginTransaction();
+    	
+    	
+    	Employee emp = new Employee("raja","Executive","Marketing");
+    	List<Address> addressList = new ArrayList<>();
+    	
+    	addressList.add(new Address("Bangaloriiii","Karnataka",60640, emp));
+    	addressList.add(new Address("Fangaloriii","Tarnataka",60641, emp));
+    	addressList.add(new Address("Kangalorii","Parnataka",60642, emp));
+    	
+    	emp.setAddress(addressList);
+    	
+    	session.persist(emp);
+    	
+    	
+    	//Address addr = new Address("Bangalori","Karnataka",60640);
+    	//Employee emp = new Employee("raja","Executive","Marketing",addr);
+    	
+		/*
+		 * The persist method is called on the Session object to save the Employee
+		 * entity (along with its associated Address) to the database. The persist
+		 * operation is not immediately executed but is queued up until the transaction
+		 * is committed.
+		 */
+		//session.persist(emp);
+    	
+    	
+    	
+    	
+    	
+  	
+//		
+//		  //this will be in uni-direction 
+//    	Address add = session.get(Address.class, 2);
+//    	System.out.println(add); // This should print the address details
+//    	Employee emp = add.getEmployee();
+//    	System.out.println(emp); // This should print the employee details
+//   	
+    	
+    	
+//    	
+//    	Address adr = session.get(Address.class, 2);
+//    	
+//    	if(adr != null) {
+//    		adr.getEmployee().setAddress(null);
+//    		session.remove(adr);
+//    	}
+    	
+		
+		System.out.println("all data printed");
+		
+		/*
+		 * This line commits the transaction, meaning all operations performed within
+		 * the transaction (like persisting the Employee entity) are finalized and saved
+		 * to the database.
+		 */
+		tx.commit();
+		
+		/*
+		 * Finally, the Session is closed to free up resources. It is essential to close
+		 * the session after the transaction is complete to prevent memory leaks and
+		 * other issues.
+		 */
+		session.close();
+		  
+    	
+    	
+    	
+    }
+}
